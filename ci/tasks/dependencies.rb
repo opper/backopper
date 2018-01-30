61df6377db62c62f0ea7defe06eda5efb2a64521
Alex <alexraileanu@me.com>
2018-01-30 12:47:05 +0100

    test with python3.5 on opper-staging server

--- ci/tasks/dependencies.rb
+++ ci/tasks/dependencies.rb
@@ -1,7 +1,7 @@
 namespace :dependencies do
     task :install do
         on roles(:app) do
-            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && pip install ."
+            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.5 venv && source venv/bin/activate && pip install ."
         end
     end
 end
