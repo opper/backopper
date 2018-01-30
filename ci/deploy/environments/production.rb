970a421b8c57ac4c31dcfe596f2815e2b0c5abba
Alex <alexraileanu@me.com>
2018-01-30 12:58:29 +0100

    deploy to acc also

--- ci/deploy/environments/production.rb
+++ ci/deploy/environments/production.rb
@@ -1,5 +1,5 @@
 server '192.81.221.208', roles: %w{app}, user: fetch(:deploy_user), port: 25642 # opper-staging
-# server '188.166.13.154', roles: %w{app}, user: fetch(:deploy_user), port: 222 # opper-acceptance
+server '188.166.13.154', roles: %w{app}, user: fetch(:deploy_user), port: 222 # opper-acceptance
 # server '188.166.77.230', user: 'deploy', roles: %w{app} # opper-live
 
 set :ssh_options, {
