76c40484afba21ef39191e6588ae6302101db563
Alex <alexraileanu@me.com>
2018-02-08 12:25:26 +0800

    fix deploy path

--- ci/tasks/dependencies.rb
+++ ci/tasks/dependencies.rb
@@ -2,7 +2,7 @@ namespace :dependencies do
     task :install do
         on roles(:dist) do
             execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py bdist_wheel --universal"
-            execute "mv #{current_path}/dist/* #{fetch(:dist_path)}/backopper"
+            execute "mv #{current_path}/dist/* #{fetch(:dist_path)}"
         end
     end
 end
