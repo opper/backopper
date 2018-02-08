36166e70275ba5e905bd2aa837b2dfe135a38a98
Alex <alexraileanu@me.com>
2018-02-08 12:18:56 +0800

    changed virtualenv python version in deploy task

--- ci/tasks/dependencies.rb
+++ ci/tasks/dependencies.rb
@@ -1,7 +1,7 @@
 namespace :dependencies do
     task :install do
         on roles(:dist) do
-            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.5 venv && source venv/bin/activate && python setup.py bdist_wheel --universal"
+            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py bdist_wheel --universal"
             execute "mv #{current_path}/dist/* #{fetch(:dist_path)}/backopper"
         end
     end
