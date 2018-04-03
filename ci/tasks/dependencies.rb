9e424e1a31f224a08d7287da1fae584dbf035942
Alex <alexraileanu@me.com>
2018-04-03 12:30:16 +0200

    rever to previous build startegy; added rotate arg to keep only the 5 last versions

--- ci/tasks/dependencies.rb
+++ ci/tasks/dependencies.rb
@@ -1,7 +1,7 @@
 namespace :dependencies do
     task :install do
         on roles(:dist) do
-            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py sdist"
+            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py bdist_wheel --universal --rotate 5"
             execute "mv #{current_path}/dist/* #{fetch(:dist_path)}"
         end
     end
