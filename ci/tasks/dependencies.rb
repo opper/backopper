9162e9c42760d95c0bf8a9db3848d7ce69ec35cb
Alex <alexraileanu@me.com>
2018-04-03 12:32:39 +0200

    fix args for rotate

--- ci/tasks/dependencies.rb
+++ ci/tasks/dependencies.rb
@@ -1,7 +1,7 @@
 namespace :dependencies do
     task :install do
         on roles(:dist) do
-            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py bdist_wheel --universal --rotate 5"
+            execute "cd #{current_path} && virtualenv --python=/usr/bin/python3.6 venv && source venv/bin/activate && python setup.py bdist_wheel --universal rotate --keep 5 --match .whl"
             execute "mv #{current_path}/dist/* #{fetch(:dist_path)}"
         end
     end
