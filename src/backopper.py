0d2c30a617f4406de19a6f62f9b8034a811e07ea
Alex <alexraileanu@me.com>
2018-08-28 16:54:04 +0200

    run crons in the bg

--- src/backopper.py
+++ src/backopper.py
@@ -125,7 +125,7 @@ def cron():
             logger.warning('Unallowed frequency came in from the API: {}, Might want to check this'.format(frequency))
             continue
 
-        cron_command = "/bin/bash -c '{0}/backup.sh {1} {0}'".format(os.environ.get('SRC_PATH'), name)
+        cron_command = "/bin/bash -c '{0}/backup.sh {1} {0}'&".format(os.environ.get('SRC_PATH'), name)
         freq = ''
 
         if frequency in allowed_frequencies:
