44d3bdcf78f5fee56c49cbae4476b2a332a08077
Alex <alexraileanu@me.com>
2019-01-25 10:30:16 +0100

    update deploy url

--- ci/deploy/environments/dist.rb
+++ ci/deploy/environments/dist.rb
@@ -1,4 +1,4 @@
-server '', roles: %w{dist}, user: fetch(:deploy_user), port: 222
+server 'pip.opper.nl', roles: %w{dist}, user: fetch(:deploy_user), port: 222
 
 # login to remote server is done via ssh-key authentication
 # never, ever with password
