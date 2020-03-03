e273dcab65d9442b49406907f61d7e1f1c73cda5
Alex <alexraileanu@me.com>
2020-03-03 11:22:49 +0100

    removed unnecessary update script

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -e bash
-
-BUCKET=$1
-
-systemctl stop backopper
-aws s3 cp s3://$BUCKET/backopper /usr/local/bin/backopper
-chmod +x /usr/local/bin/backopper
-systemctl start backopper
